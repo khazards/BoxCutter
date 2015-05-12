d6e97cfa8b9d3c18bae5aa67c9630cb5394a5675
unknown <sharrielovestempest@gmail.com>
2015-05-12 13:16:30 -0600

    asd

---
+++ Assets/scripts/Box.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Box : MonoBehaviour {
+
+	public Color defaultColor = Color.red;
+	public Color selectedColor =  Color.green;
+	public Renderer mat;
+
+	void Start() {
+		mat = GetComponent<Renderer>();
+	}
+
+    void OnTouchDown(){
+		mat.material.color = selectedColor;
+	}
+	void OnTouchUp(){
+		mat.material.color = defaultColor;
+	}
+	void OnTouchStay(){
+		mat.material.color = selectedColor;
+	}
+	void OnTouchExit(){
+		mat.material.color = defaultColor;
+	}
+}
---
+++ Assets/scripts/ScaleWithCanvas.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleWithCanvas : MonoBehaviour {
+
+	public GameObject MainCanvas;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+	}
+}
